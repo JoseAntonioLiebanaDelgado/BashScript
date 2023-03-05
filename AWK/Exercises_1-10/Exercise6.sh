d2e0d3b5f07d0d08c33ddf0cf39cab1d4cc03d5a
Jose Antonio Liebana Delgado <122827485+JoseAntonioLiebanaDelgado@users.noreply.github.com>
2023-03-05 21:26:36 +0100

    Update Exercise6.sh

--- AWK/Exercises_1-10/Exercise6.sh
+++ AWK/Exercises_1-10/Exercise6.sh
@@ -3,29 +3,32 @@
 #Per fer aquest script aprofita l'anterior, i dissenya una funcio que ens digui#si un nombre és o no primer.
 
 esPrimer(){
-primer=1
-
-for i in $(seq 2 $(($1-1)))
-do
-	if [ $(($1%$i)) -eq 0 ]; then
-		primer=0
-		break
-	fi
-done
-
-return $primer
-
+    if [ $1 -eq 2 ]; then
+        return 1
+    elif [ $(($1 % 2)) -eq 0 ]; then
+        return 0
+    fi
+    
+    for ((i=3; i<$1; i+=2))
+    do
+        if [ $(($1 % $i)) -eq 0 ]; then
+            return 0
+        fi
+    done
+    
+    return 1
+}
 
 numero=1
 recompte=0
 
-while [ $recompte -le 1000 ]
+while [ $recompte -lt 1000 ]
 do
-	numero=$(($numero+1))
-	esPrimer $numero
-	if [ $? -eq 1 ]; then
-		recompte=$(($recompte+1))
-		echo -n "$numero, "
-	fi
-
+    numero=$(($numero+1))
+    esPrimer $numero
+    if [ $? -eq 1 ]; then
+        recompte=$(($recompte+1))
+        echo -n "$numero, "
+    fi
 done
+
